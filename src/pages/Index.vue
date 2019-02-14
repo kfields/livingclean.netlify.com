ea1c13c6f1f2c0ffb6123fb65b2a2fe96ec4d295
Kurtis Fields <kurtisfields@gmail.com>
2019-02-14 11:29:37 -0500

    Better section spacing

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,20 @@
 <template>
   <Layout>
     <HeroBanner :data="this.data" />
-    <section class="section">
-      <p>
-        Here at living clean we believe in a safe structured sober living environment.
-        We encapsulate the proven time tested method of the 12 steps of Alcoholics Anonymous In all that we do.
-        We create a social support structure for all residing in the house, and it provides a sense that the person is not alone, thus helping improve motivation to remain sober.
-        The camaraderie experience here at Living Clean is unlike any other.
-        Residents form a long lasting bond that quickly becomes a brotherhood.
-        We believe in personal growth through unity, service to those who still suffer, and accountability to one another that reflects a new way of living for each and every resident that is on this journey together.
-        At Living Clean we are staying sober one day at a time and having fun doing it!
-      </p>
-    </section>
-    <RecentArticles />
+    <main>
+      <section class="section">
+        <p>
+          Here at living clean we believe in a safe structured sober living environment.
+          We encapsulate the proven time tested method of the 12 steps of Alcoholics Anonymous In all that we do.
+          We create a social support structure for all residing in the house, and it provides a sense that the person is not alone, thus helping improve motivation to remain sober.
+          The camaraderie experience here at Living Clean is unlike any other.
+          Residents form a long lasting bond that quickly becomes a brotherhood.
+          We believe in personal growth through unity, service to those who still suffer, and accountability to one another that reflects a new way of living for each and every resident that is on this journey together.
+          At Living Clean we are staying sober one day at a time and having fun doing it!
+        </p>
+      </section>
+      <RecentArticles />
+    </main>
   </Layout>
 </template>
 
